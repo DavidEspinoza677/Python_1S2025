8c189b41e50c050af6a186deba7866312f6af512
DavidEspinoza677 <davespinoza@uamv.edu.ni>
2025-03-19 10:00:25 -0600

    Sesion 1 17032025

---
+++ sesion 1 17032025/exameple1.py
@@ -0,0 +1,10 @@
+#Comentario de una linea 
+"""Comentario de varias lineas"""
+
+
+print("Datros del estudiante")
+print("="*20)
+print("Nombre: " , end="")
+print("Jose Alejandro")
+print("Apellido: " , end="")
+print("Duran Garcia")
